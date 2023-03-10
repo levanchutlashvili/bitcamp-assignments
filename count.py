6bac65d4311776a7f74d3c4890d6ad655a15a90a
levanchutlashvili <127182221+levanchutlashvili@users.noreply.github.com>
2023-03-10 22:16:18 +0400

    Add files via upload

---
+++ count.py
@@ -0,0 +1,9 @@
+#count the number of characters
+
+text = input("Please enter some text: ")
+
+
+if not text:
+    print("Please enter some text")
+
+print("The number of characters  is:", len(text)) 
