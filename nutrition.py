8d1e7e3f895e9998dec8a5e15c20c07a187679b7
levanchutlashvili <127182221+levanchutlashvili@users.noreply.github.com>
2023-03-15 15:57:47 +0400

    Add files via upload

---
+++ nutrition.py
@@ -0,0 +1,11 @@
+calories = {
+    "Apple" : 130,
+    "Avocado" : 50,
+    "Sweet cherries" : 100,
+    "Banana" : 140,
+    "Strawberries" : 30,
+    "Lemon" : 25,
+    "Orange" : 15
+}
+
+print(f"Calories: ", calories[input("Items: ").title()])
